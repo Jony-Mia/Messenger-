2c7d596331c6de2c61e06f24c77adc4629d2e322
ajonymia321@gmail.com <ajonymia321@gmail.com>
2024-01-25 16:24:15 +0600

    Messenger

--- chat.js
+++ chat.js
@@ -1,8 +1,38 @@
-/* let chat = document.getElementById('chat');
-chat.onclick = () => {
-  let a=document.createElement('a')
-  a.href="/index.html"
-  a.textContent='Hello'
-  chat.appendChild(a)
+let btn = document.getElementById('btn');
+let btn2 = document.getElementById('hide');
+let bar = document.querySelectorAll('#bar li');
+let offcanvas = document.getElementById('offcanvas');
+
+function clik() {
+  window.open('/html/chatBox.html')
 }
-*/
+function click() {
+
+  bar.forEach((list) => {
+    list.classList.remove('active');
+    this.classList.add('active');
+  })
+}
+bar.forEach((item) => {
+  item.addEventListener('click', click)
+})
+/*
+if (btn2.click) {
+
+  btn2.onclick = () => {
+
+    offcanvas.style.transform = "translateX(-400px)";
+    offcanvas.style.display = "none";
+  }
+  if (btn.click) {
+      offcanvas.style.display = "block";
+
+  }
+
+}
+/* (btn.click) else{
+btn.onclick=()=>{
+  let offcanvas=document.getElementById('offcanvas');
+  offcanvas.style.transform="translateX(400px)";
+} 
+}*/
--- html/main.js
+++ html/main.js
@@ -2,37 +2,44 @@ let send = document.getElementById('send');
 let p = document.getElementById('p');
 let em = document.getElementById('em');
 let text = document.getElementById('text')
+let microphone = document.getElementById('microphone')
+/*sed.addEventListener('click',()=>{})*/
+let speech=new SpeechSynthesisUtterance();
 
-
-/*sed.addEventListener('click',()=>{
-
-})*/
 text.addEventListener('click', () => {
   if (em.classList.contains('fa-face-smile')) {
     em.classList.remove('fa-face-smile');
     em.classList.add('fa-paper-plane');
     send.addEventListener('click', sent)
+  }
+  /*else {
+     em.classList.add('fa-face-smile');
+     em.classList.remove('fa-paper-plane');
 
-  } /*else {
-    em.classList.add('fa-face-smile');
-    em.classList.remove('fa-paper-plane');
-
-    send.removeEventListener('click', sent)
-  }*/
+     send.removeEventListener('click', sent)
+   }*/
 })
 
 function sent() {
-  let txt = document.createElement('p');
-  txt.className ='m-3 bg-info text-light rounded p-1';
+speech.text=text.value;
+  speech.lang='en';
+  window.speechSynthesis.speak(speech)
   
+  
+  
+  let txt = document.createElement('p');
+  txt.className = 'm-3 bg-info text-light rounded p-1';
 
+  if (text.value) {
+      let text = document.getElementById('text')
 
-if (text.value) {
-  txt.textContent = text.value;
+    txt.textContent = text.value;
     p.appendChild(txt)
-
+  }
+  text.value = ''
 }
-text.value=''
 
+function close() {
+  window.open('../index.html')
 }
 
